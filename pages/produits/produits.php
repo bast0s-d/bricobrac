608488aae70e1135e9fb699ff36758b012af3896
bast0s-d <hecklen.bastien68@gmail.com>
2025-10-21 15:10:18 +0200

    add DB connection
    Signed-off-by: bast0s-d <hecklen.bastien68@gmail.com>

--- pages/produits/produits.php
+++ pages/produits/produits.php
@@ -1,3 +1,18 @@
-<div class="">
+<?php
+require_once 'src/DBConnection.php';
+
+
+$sql = "SELECT * FROM produits";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+// PDO::FETCH_ASSOC garantit que les clés du tableau sont les noms des colonnes (id, nom, reference, etc.)
+$produits = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+dump($produits);
+?>
+
+<div class="Produits container">
 
 </div>
